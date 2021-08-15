64cd03df17fbb5050b8e94c02509dadc875ee4d8
Мария Рожко <88270532+MeriSeverina@users.noreply.github.com>
2021-08-15 23:34:04 +0300

    Added Hello World class
    Added Hello World class

---
+++ Lesson1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Lesson1 {
+    public static void main(String[] args) {
+        System.out.println("Hello, team");
+                Scanner sc = new Scanner(System.in);
+                String name = sc.nextLine();
+                System.out.println("Hello " + name);
+    }
+}
